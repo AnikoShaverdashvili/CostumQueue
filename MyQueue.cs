ad49556d7b0823887c81ebaa0c586fcef0e37aee
AnikoShaverdashvili <shaverdashviliania@gmail.com>
2022-11-12 19:47:44 +0400

    Add project files.

---
+++ MyQueue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostumQueue
+{
+    public class MyQueue<T>
+    {
+        private readonly List<T>? _queue;
+        private int i;
+
+        public MyQueue(List<T>? queue)
+        {
+            _queue = queue;
+        }
+
+        public MyQueue(int capacity)
+        {
+            _queue = new List<T>(capacity);
+        }
+
+       
+        public void Enqueue(T element)
+        {
+            _queue!.Add(element);
+        }
+
+       
+        public T Dequeue ()
+        {
+            if (_queue == null || _queue?.Count == 0)
+            {
+                throw new IndexOutOfRangeException("Queue is empty");
+            }
+
+            T element = _queue![i];
+            i++;
+
+            return element;
+        }
+
+        public MyQueue()
+        {
+            _queue = new List<T>();
+        }
+
+        public int Size()
+        {
+            if (_queue == null)
+            {
+                return 0;
+            }
+
+            return _queue.Count;
+        }
+
+        public bool IsEmpty()
+        {
+            return _queue?.Count == 0;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using CostumQueue;
+using static System.Net.Mime.MediaTypeNames;
+
+
+
+MyQueue<int> queue = new MyQueue<int>();
+
+Console.Write("check if queue is empty: ");
+bool res1 = queue.IsEmpty();
+Console.WriteLine(res1);
+
+
+queue.Enqueue(10);
+queue.Enqueue(30);
+queue.Enqueue(50);
+queue.Enqueue(70);
+
+Console.Write("check if queue is empty after add elements: ");
+bool res= queue.IsEmpty();
+
+Console.WriteLine(res);
+
+
+Console.Write("Queue size is : ");
+int? queueSize = queue.Size();
+Console.WriteLine(queueSize);
+
+
+
+Console.WriteLine("Deleted elements are : ");
+
+int delEl=queue.Dequeue();
+Console.WriteLine(delEl);
+
+int delEl2=queue.Dequeue();
+Console.WriteLine(delEl2);
+
+int delEl3=queue.Dequeue();
+Console.WriteLine(delEl3);
+
+
+
